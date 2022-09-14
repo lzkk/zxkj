95fc7c0917ab31805194e7fd6dbd52e4d9dfe77b
yuhui <yuhui@winzct.com>
2022-09-14 18:59:38 +0800

    xxlJobAdmin优化

--- infrastructure-parent/xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ infrastructure-parent/xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,5 +1,6 @@
 package com.xxl.job.admin;
 
+import com.xxl.job.core.config.Config;
 import com.zxkj.xxl.XxlJobConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
-@SpringBootApplication(exclude = XxlJobConfig.class)
+@SpringBootApplication(exclude = {XxlJobConfig.class, Config.class})
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
